dfe16040df0e07dcb1b3fd89af86ab3952c12843
Akos032 <horako032@hengersor.hu>
2025-02-26 09:22:51 +0100

    card

--- mestermunka/gyak/react/mestermunka (V2)/src/components/pages/Home.jsx
+++ mestermunka/gyak/react/mestermunka (V2)/src/components/pages/Home.jsx
@@ -8,9 +8,6 @@ export const Home =() => {
     const showELement = true;
     const [data,setData] = useState([])
     const [input,setInput] = useState("");
-    function submint(e){
-        e.showELement = false
-    }
 
 
     useEffect(() => {
@@ -24,25 +21,30 @@ export const Home =() => {
     <>
 
     <div className="row">
-        <div className="col-sm-3">
+        <div className="col-sm-4">
             <input type="text" id="ingredients" placeholder="Írja be a recept nevét..." value={input} onChange={(e) => setInput(e.target.value)}/>
             <input type="text" id="ingredients" placeholder="Írja be a hozzávalót..."/>
         </div>
-        <div className="col-sm-6">
-            <table>
-                <tbody>
+        <div className="col-sm-4">
                 {data.map((d) =>
-                    <tr id='Box' key={data.Receptek_id} style={{fontSize:"15px"}}>
-                        <img src={d.kep} id='img' style={{maxWidth:"300px", height:"auto" }}/>
-                        <th id='name'>{d.Receptek_neve}</th>
-                        { showELement && <td id='recept'>{d.Keszites}</td>}
-                    </tr>
+                   <div className="card mb-3" style={{maxWidth: "800px",height:"auto"}}>
+                        <div className="row g-0" key={data.Receptek_id}>
+                            <div className="col-md-4">
+                                <img src={d.kep} className="img-fluid rounded-start"/>
+                            </div>
+                        <div className="col-md-8">
+                            <div className="card-body">
+                                <h5 className="card-title">{d.Receptek_neve}</h5>
+                                <p className="card-text">{d.Keszites.substring(0,200)}</p>
+                                <button type="button">Több</button>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
                 )}
-                </tbody>
-            </table>
         </div>
-        <div className="col-sm-3">
-            <h1 style={{textAlign:"center"}}>Rólunk</h1>
+        <div className="col-sm-4">
+            <h1 style={{textAlign:"center", margin:"5px"}}>Rólunk</h1>
         </div>
     </div>
      
--- orai feladat/backend/index.js
+++ orai feladat/backend/index.js
@@ -9,7 +9,7 @@ app.use(cors())
 const db = mysql.createConnection({
     user:"root",
     host: "127.0.0.1",
-    port: 3306,
+    port: 3307,
     password: "",
     database: "fogado",
 })
@@ -21,6 +21,22 @@ app.get("/" , (req,res) => {
 app.get("/szobak", (req,res) =>{
     const sql = "SELECT szobak.sznev, szobak.agy FROM `szobak`";
     db.query(sql, (err,result) =>{
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+app.get("/vendegek", (req,res) =>{
+    const sql = "SELECT szobak.sznev, Sum(foglalasok.fo), Sum((foglalasok.tav-foglalasok.erk)*foglalasok.fo) FROM `szobak` inner join foglalasok on szobak.szazon = foglalasok.szoba GROUP by szobak.sznev;";
+    db.query(sql, (err,result) =>{
+        if(err) return res.json(err);
+        return res.json(result)
+    })
+})
+
+app.get("/vendegek/:id", (req,res) =>{
+    const sql = "SELECT vendegek.vnev, foglalasok.erk, foglalasok.tav FROM szobak inner join foglalasok on szobak.szazon = foglalasok.szoba inner join vendegek on foglalasok.vendeg = vendegek.vsorsz WHERE szobak.szazon = ?;";
+    db.query(sql, [req.params.id], (err,result) =>{
         if(err) return res.json(err);
         return res.json(result)
     })
