20debaf4b27d9019da71eb7bf792da0d55315977
Akos032 <horako032@hengersor.hu>
2025-03-04 12:25:46 +0100

    Update Recept.jsx

--- mestermunka/gyak/react/mestermunka (V2)/src/components/pages/Recept.jsx
+++ mestermunka/gyak/react/mestermunka (V2)/src/components/pages/Recept.jsx
@@ -3,27 +3,57 @@ import { useState, useEffect } from "react";
 const Recept = () => {
   const [kereses, setKereses] = useState("");
   const [hozzavalok, setHozzavalok] = useState([]);
+  const [eredmeny, setEredmeny] = useState([]);
+  const [toltes, setToltes] = useState([false])
 
   useEffect(() => {
-    fetch(`http://localhost:3001/api/hozzavalok?keres=${kereses}`)
-      .then(res => res.json())
-      .then(data => setHozzavalok(data))
-      .catch(err => console.error(err));
-  }, [kereses]);
+    if(kereses === "") {
+      setEredmeny([]);
+      setToltes(false);
+      return;
+    }
+  })
 
-  return (
+const ido = setTimeout(() => {
+  setToltes(true);
+  useEffect(() => {
+    fetch(`http://localhost:3001/api/hozzavalok?keres=${kereses}`)
+      .then((res) => res.json())
+      .then(data => {
+        const filter = data.filter((hozzavalok) => 
+        hozzavalok.Hozzavalok_neve.toLowerCase().includes(kereses.toLowerCase())
+        );
+        setEredmeny(filter)
+        setToltes(false);
+      } )
+      .catch(() => {
+        setToltes(false);
+      })
+      return () => clearTimeout(ido)
+  }, [kereses]);
+})
+
+const handel = (e) => {
+  const value = e.target.value;
+  setKereses(value)
+}
+
+return (
     <div>
       <input
         type="text"
         placeholder="Keresés..."
         value={kereses}
-        onChange={(e) => setKereses(e.target.value)}
+        onChange = {handel}
       />
-      <ul>
-        {hozzavalok.map((hozzavalo) => (
-          <li key={hozzavalo.id}>{hozzavalo.Hozzavalok_neve}</li>
-        ))}
-      </ul>
+      {kereses && <p>Keresés: <strong>{kereses}</strong></p>}
+      {toltes && <p>Töltés...</p>}
+      <div>
+        {eredmeny.length > 0 ? (
+          eredmeny.map((hozzavalok) => <div key = {hozzavalok.Hozzavalok_id}></div>)
+        ) : (kereses && !toltes && <p>Nincs válasz</p>
+        )}
+      </div>
     </div>
   );
 };
