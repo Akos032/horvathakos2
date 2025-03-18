9f332c3357a9b80a4bb10f0d8c5b60ca9b573314
Akos032 <horako032@hengersor.hu>
2025-03-18 11:03:54 +0100

    navbar

--- mestermunka/gyak/react/mestermunka (V2)/src/components/Navbar.jsx
+++ mestermunka/gyak/react/mestermunka (V2)/src/components/Navbar.jsx
@@ -1,35 +1,30 @@
-import React from "react";
-import {useState} from "react";
-import {Link, NavLink} from "react-router-dom"
-import "./Navbar.css"
+import React, { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import "./Navbar.css";
 
-const NavBar = (() => {
-    const [menuOpen,setMenuOpen] = useState(false)
-    return(
-        <nav>
-            <div>
-           
-                <Link to = '/' className="title">Receptek</Link>
-                <div className="menu" onClick={() =>
-                setMenuOpen(!menuOpen)
-                }>
-                <span></span>
-                <span></span>
-                <span></span>
+const NavBar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <nav id="navbar">
+            <div id="nav-header">
+                <div id="logo-title">
+                    <img src={"Média.png"} alt="Logo" id="nav-logo" />
+                    <Link to='/' id="nav-title">Receptek</Link>
                 </div>
-            
+                <div id="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
             </div>
-            <ul className={menuOpen ? "open" : ""}>
-            <li>
-                <NavLink to = "/Login">Bejeletkezés</NavLink>
-                <NavLink to = "/Recept">Receptek</NavLink>
-                <NavLink to = "/Profile">Profil</NavLink>
-            </li>
-
+            <ul id="nav-links" className={menuOpen ? "open" : ""}>
+                <li><NavLink to="/Login">Bejeletkezés</NavLink></li>
+                <li><NavLink to="/Recept">Receptek</NavLink></li>
+                <li><NavLink to="/Profile">Profil</NavLink></li>
             </ul>
         </nav>
+    );
+};
 
-    )
-})
-
-export default NavBar
+export default NavBar;
