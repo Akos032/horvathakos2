cc1baf46b60a5c543ec83bd6298712c65101998e
Akos032 <horako032@hengersor.hu>
2025-03-24 10:03:13 +0100

    Revert "dizájn"
    This reverts commit 3336f0b226d013661fb77710b08466f6d7263c0b.

--- mestermunka/gyak/react/mestermunka (V2)/src/components/Navbar.jsx
+++ mestermunka/gyak/react/mestermunka (V2)/src/components/Navbar.jsx
@@ -1,59 +1,38 @@
-import React, { useState } from 'react';
+import React, {useState,useEffect} from 'react';
 import { Link, NavLink } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.css';
 
 const NavBar = ({ isLoggedIn, onLogout }) => {
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <nav id="navbar" className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div className="container-fluid">
-        <div id="logo-title" className="d-flex align-items-center gap-2">
+    <nav id="navbar">
+      <div id="nav-header">
+        <div id="logo-title">
           <img src={"Média.png"} alt="Logo" id="nav-logo" />
-          <Link to="/" id="nav-title" className="navbar-brand">Receptek</Link>
+          <Link to="/" id="nav-title">Receptek</Link>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded={menuOpen ? "true" : "false"}
-          aria-label="Toggle navigation"
-          onClick={() => setMenuOpen(!menuOpen)}
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className={`collapse navbar-collapse ${menuOpen ? "show" : ""}`} id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            {/* Always show Login link if not logged in */}
-            {!isLoggedIn && (
-              <li className="nav-item">
-                <NavLink to="/login" className="btn-rounded btn-nav">Bejelentkezés</NavLink>
-              </li>
-            )}
-
-            {/* Only show Recept and Profile after login */}
-            {isLoggedIn && (
-              <>
-                <li className="nav-item">
-                  <NavLink to="/recept" className="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-black">Receptek</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/profile" className="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-near-black">Profil</NavLink>
-                </li>
-                <li className="nav-item">
-                  <NavLink to="/finomsagok" className="f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-near-black">Finomságok</NavLink>
-                </li>
-                <li className="nav-item">
-                  <button className='f6 grow no-underline br-pill ph3 pv2 mb2 dib white bg-near-black' onClick={onLogout}>Kilépés</button>
-                </li>
-              </>
-            )}
-          </ul>
+        <div id="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
+          <span></span>
+          <span></span>
+          <span></span>
         </div>
       </div>
+      <ul id="nav-links" className={menuOpen ? "open" : ""}>
+        {/* Always show Login link if not logged in */}
+        {!isLoggedIn && <li><NavLink to="/login">Bejelentkezés</NavLink></li>}
+
+        {/* Only show Recept and Profile after login */}
+        {isLoggedIn && (
+          <>
+            <li><NavLink to="/recept">Receptek</NavLink></li>
+            <li><NavLink to="/profile">Profil</NavLink></li>
+            <li>
+              <button onClick={onLogout}>Logout</button>
+            </li>
+          </>
+        )}
+      </ul>
     </nav>
   );
 };
